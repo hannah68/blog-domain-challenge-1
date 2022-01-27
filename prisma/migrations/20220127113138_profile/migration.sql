5bc1b156a2e39a82951c6e9a3626b5a695761b66
hannah68 <reihaneh.naderi1989@gmail.com>
2022-01-27 17:24:14 +0000

    first requirment completed

---
+++ prisma/migrations/20220127113138_profile/migration.sql
@@ -0,0 +1,48 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "bio" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "comment" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "postId" INTEGER NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.js
+++ prisma/seed.js
@@ -2,20 +2,53 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdUsers = await prisma.user.createMany({
-        data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
-        ]
+    const createdUsers = await prisma.user.create({
+        data: { 
+            username: 'alicemartin',
+            profile: {
+                create: {
+                    bio: 'manager from london',
+                }
+            }
+        },
     });
-
-    console.log(`${createdUsers.count} users created`, createdUsers);
-
-    // Add your code here
+    console.log('users created', createdUsers);
 
     
+    // create post==================================
+    const createPost = await prisma.post.create({
+        data:{
+                title: '10 things you need to know about web development',
+                post: ' the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centurie',
+                user: {
+                    connect: {
+                        id: createdUsers.id
+                    }
+                }
+        }
+    })
+    console.log('post created', createPost);
 
+    // create comment==================================
+    const createComment = await prisma.comment.create({
+        data: {
+            content: 'pposed to using content here, making it look like readable English',
+            user: {
+                connect: {
+                    id: createdUsers.id
+                }
+            },
+            post: {
+                connect: {
+                    id: createPost.id
+                }
+            }
+        }
+    })
+    console.log('comment created', createComment);
 
+   
+    
     // Don't edit any of the code below this line
     process.exit(0);
 }
